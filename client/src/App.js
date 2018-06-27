9e4fc4f9174fc288efc66d476a154eeded12a41e
davidmd <davidmdugger@gmail.com>
2018-06-26 20:09:29 -0600

    pulled JSON Web Token from localStorage use globally

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 import store from "./store";
 
 import "./App.css";
 
 import Layout from "./components/Layout/Layout";
+
+const token = localStorage.jwtToken;
+if (token) {
+  // set auth token header
+  setAuthToken(token);
+  // decode token and get user info
+  const decodedUser = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decodedUser));
+}
 
 class App extends Component {
   render() {
