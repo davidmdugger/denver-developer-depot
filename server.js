d9b7f6845d3d520a9c731c9fb5bdab12452ef4bb
davidmd <davidmdugger@gmail.com>
2018-07-04 11:25:10 -0600

    added index.html to build

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
