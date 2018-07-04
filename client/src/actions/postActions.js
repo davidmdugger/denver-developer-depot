91902bf822aecb44abea969558ea145efbead4b3
davidmd <davidmdugger@gmail.com>
2018-07-04 08:25:17 -0600

    added error message removal

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,11 +5,14 @@ import {
   GET_POSTS,
   GET_POST,
   POST_LOADING,
-  DELETE_POST
+  DELETE_POST,
+  CLEAR_ERRORS
 } from "./types";
 
 // add post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
+
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -110,6 +113,7 @@ export const removeLike = id => dispatch => {
 
 // add comment
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
     .then(res =>
@@ -147,5 +151,12 @@ export const deleteComment = (postId, commentId) => dispatch => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  };
+};
+
+// clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,3 +15,6 @@ export const GET_POSTS = "GET_POSTS";
 export const GET_POST = "GET_POST";
 export const ADD_POST = "ADD_POST";
 export const DELETE_POST = "DELETE_POST";
+
+// clear errors
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
